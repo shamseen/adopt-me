da0b8ff40f383cd4aec7cd6334bf99a869ca275e
shamseen <shamseen.c@gmail.com>
2020-11-08 18:21:31 -0500

    made a pet component to put in App stamp

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
+
 // creating a "rubber stamp" but not using it
+const Pet = () => {
+    return React.createElement("div", {}, [
+        React.createElement('h1', {}, 'Naabi'),
+        React.createElement('h2', {}, 'Cat'),
+        React.createElement('h2', {}, 'Korat?'),
+    ])
+}
+
 const App = () => {
 
-    // stamps div containing an h1
+    // stamps div containing an h1 and pet components
     // params: tag, attributes, children
     return React.createElement(
         "div",
         { id: 'logo' },
-        React.createElement("h1", {}, "Adopt Me!")
-    );
+        [
+            React.createElement("h1", {}, "Adopt Me!"),
+            React.createElement(Pet),
+            React.createElement(Pet),
+            React.createElement(Pet)
+        ]);
 }
 
 // rendering to div we named root - using the stamp
